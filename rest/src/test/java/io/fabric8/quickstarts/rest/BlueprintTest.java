96145b198b9f8813b85e0530fca4933737228ecc
pavluk <pavlukan1982@gmail.com>
2015-12-10 16:55:57 +0300

    Don't work

--- rest/src/test/java/io/fabric8/quickstarts/rest/BlueprintTest.java
+++ rest/src/test/java/io/fabric8/quickstarts/rest/BlueprintTest.java
@@ -1,12 +1,18 @@
 package io.fabric8.quickstarts.rest;
 
 import org.apache.camel.test.blueprint.CamelBlueprintTestSupport;
+import org.apache.cxf.jaxrs.client.WebClient;
 import org.junit.Test;
+
+import javax.ws.rs.core.Response;
 
 /**
  * Created by pavlyukevich on 09.12.2015.
  */
 public class BlueprintTest extends CamelBlueprintTestSupport {
+
+    private static final String ENDPOINT_ADRESS = "http://localhost:8181/cxf/crm";
+    public static final String CUSTOMER_TEST_PATH = "/customerservice/customers/123";
 
     @Override
     protected String getBlueprintDescriptor() {
@@ -15,6 +21,11 @@ public class BlueprintTest extends CamelBlueprintTestSupport {
 
     @Test
     public void endpontTest(){
-        String n = "1";
+
+        WebClient client = WebClient.create(ENDPOINT_ADRESS);
+        client.accept("media/jpeg");
+        client.path(CUSTOMER_TEST_PATH);
+//        Response response = client.get();
     }
+
 }
